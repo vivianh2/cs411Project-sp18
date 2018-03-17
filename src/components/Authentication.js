407ef96e1ec67ed992fed73a620dc927d575044e
cherylqian <cherylqian@outlook.com>
2018-03-17 14:46:08 -0700

    refactored

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import GoogleLogin from 'react-google-login';
-import { GoogleLogout } from 'react-google-login';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
 // this component should handle
 // 1. login and logout
@@ -8,75 +7,70 @@ import { GoogleLogout } from 'react-google-login';
 
 class Authentication extends Component {
   state = {
-    loginStatus: 'no', // Yes or no
-    logbarWord: 'Google Login', // What to show on log bar
-    netId: 'unknow', // User netId
-    userName: 'unknow'
+    status: 0, // Yes or no
+    prompt: 'Google Login', // What to show on log bar
+    netId: 'unknown', // User netId
+    userName: 'unknown'
   };
 
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
+  constructor(){
+    super();
+    this.isLoggedIn = this.isLoggedIn.bind(this);
+    this.loginResponse = this.loginResponse.bind(this);
+    this.logoutResponse = this.logoutResponse.bind(this);
   }
 
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    return body;
+  isLoggedIn(){
+    return this.state.status;
+  }
+
+  loginResponse(GoogleUser) {
+      var email = GoogleUser.getBasicProfile().getEmail();
+      var isStudent = email.endsWith('@illinois.edu')
+      if(isStudent){
+          var atIndex = email.indexOf('@');
+          var netId = email.substring(0, atIndex);
+          var userName = GoogleUser.getBasicProfile().getName();
+          this.setState(
+            { prompt: 'Log out ' + userName,
+              netId: netId,
+              userName: userName,
+              status: 1
+            }
+          );
+      }else{
+          alert("Please use UIUC email~");
+      }
+  };
+
+  logoutResponse(GoogleUser) {
+    this.setState(
+      {
+        status: 0,
+        prompt: 'Google Login',
+        netId: 'unknown',
+        userName: 'unknown'
+      }
+    );
+    alert("Logged out! We'll miss you~");
   };
 
   render() {
-    const loginResponse = (GoogleUser) => {
-        var email = GoogleUser.getBasicProfile().getEmail();
-        var isStudent = email.endsWith('@illinois.edu')
-        if(isStudent == true){
-            var atIndex = email.indexOf('@');
-            var netId = email.substring(0, atIndex);
-            var userName = GoogleUser.getBasicProfile().getName();
-            this.setState(
-              { logbarWord: 'Log out ' + userName,
-                netId: netId,
-                userName: userName,
-                loginStatus: 'yes'
-              }
-            );
-        }else{
-            alert("Please use UIUC email~");
-        }
-    };
-
-    const logoutResponse = (GoogleUser) => {
-      this.setState(
-        { logbarWord: 'Google Login',
-          netId:'unknow',
-          userName: 'unknow',
-          loginStatus: 'no'
-        }
-      );
-      alert("Logged out! We'll miss you~");
-    };
-
-    if(this.state.loginStatus == 'no'){
-      return (
-        <div>
-          <GoogleLogin
-            clientId="609372741285-p2k1ujp7bdbc5l05oc47102uvoc40qpb.apps.googleusercontent.com"
-            buttonText={this.state.logbarWord}
-            onSuccess={loginResponse}
-            onFailure={loginResponse}
-          />
-        </div>
-      );
-    }
     return (
-        <div>
+      <div className="auth">
+      { !this.isLoggedIn() ?
+        <GoogleLogin
+          clientId="609372741285-p2k1ujp7bdbc5l05oc47102uvoc40qpb.apps.googleusercontent.com"
+          buttonText={this.state.prompt}
+          onSuccess={this.loginResponse}
+          onFailure={this.loginResponse}
+        />
+        :
         <GoogleLogout
-          buttonText="Logout"
-          onLogoutSuccess={logoutResponse}
-        >
-        </GoogleLogout>
+          buttonText={this.state.prompt}
+          onLogoutSuccess={this.logoutResponse}
+        />
+      }
       </div>
     );
 
