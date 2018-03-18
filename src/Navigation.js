b8b4d2abe40ee27fed7c1dcd0f6f6cad3926f58f
cherylqian <cherylqian@outlook.com>
2018-03-18 10:19:14 -0700

    router done

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,18 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+
 import { Link } from 'react-router-dom';
 
 import Authentication from './components/Authentication'
 
-class Navigation extends Component {
-  render(){
-    return (
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/Account'>Account</Link></li>
-        <li><Authentication /></li>
-      </ul>
-    );
-  }
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+function Navigation(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="title" color="inherit" className={classes.flex}>
+            <Link to='/'>Read Me Again</Link>
+          </Typography>
+          <Button component={({...props}) => <Link to='/account' {...props} />}>Account</Button>
+          <Authentication />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
-export default Navigation;
+Navigation.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Navigation);
