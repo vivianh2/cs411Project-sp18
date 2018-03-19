5f9d1f9472fc1b5e358d7475a39b3a197e2ed973
cherylqian <cherylqian@outlook.com>
2018-03-19 15:48:04 -0700

    update styling

--- src/index.js
+++ src/index.js
@@ -9,12 +9,25 @@ import Navigation from './Navigation';
 import App from './components/App';
 import Account from './components/Account'
 
+import CssBaseline from 'material-ui/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import blue from 'material-ui/colors/blue';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+  },
+});
+
 ReactDOM.render((
   <Router>
     <div>
-      <Navigation />
-      <Route exact path="/" component={App}/>
-      <Route path="/account" component={Account}/>
+      <CssBaseline />
+      <MuiThemeProvider theme={theme}>
+        <Navigation />
+        <Route exact path="/" component={App}/>
+        <Route path="/account" component={Account}/>
+      </MuiThemeProvider>
     </div>
   </Router>
 ), document.getElementById('root'));
