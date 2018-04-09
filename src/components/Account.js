7ff761b2366123b175177dd6e127a89d9312636a
cherylqian <cherylqian@outlook.com>
2018-04-08 21:39:33 -0500

    init

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,15 +1,37 @@
 import React, { Component } from "react";
-import Typography from 'material-ui/Typography';
-import Grid from 'material-ui/Grid';
-import Ratings from './Ratings';
-import History from './History'
+import { withStyles } from "material-ui/styles";
+import Typography from "material-ui/Typography";
+import Grid from "material-ui/Grid";
+import Paper from "material-ui/Paper";
+import { MenuList, MenuItem } from "material-ui/Menu";
+import { ListItemIcon, ListItemText } from "material-ui/List";
+import AccountIcon from "material-ui-icons/AccountCircle";
+
+import Ratings from "./Ratings";
+import History from "./History";
+
+const styles = theme => ({
+  menuItem: {
+    "&:focus": {
+      backgroundColor: theme.palette.primary.main,
+      "& $primary, & $icon": {
+        color: theme.palette.common.white
+      }
+    }
+  },
+  primary: {},
+  icon: {},
+  menu: {
+    height: "100%",
+  }
+});
 
 class Account extends Component {
   state = {
     netid: this.props.location.state.netid,
     username: this.props.location.state.username,
     rating: 0
-  }
+  };
 
   componentDidMount() {
     this.getAccount(this.state.netid)
@@ -30,16 +52,50 @@ class Account extends Component {
   };
 
   render() {
+    const { classes } = this.props;
     return (
       <Grid container justify="flex-start" alignItems="center">
-        <Typography variant="headline" align='left' color="inherit"  style={{ margin: "1% 2%" }}>
-          {this.state.username}
-        </Typography>
-        <Ratings rating={this.state.rating} />
-        <History netid={this.state.netid} />
+        <Grid item xs={12}>
+          <Typography
+            variant="headline"
+            align="left"
+            color="inherit"
+            style={{ margin: "1% 2%" }}
+          >
+            {this.state.username}
+          </Typography>
+          <Ratings rating={this.state.rating} />
+        </Grid>
+        <Grid item xs={12}>
+          <Paper>
+            <MenuList className={classes.menu}>
+              <MenuItem className={classes.menuItem}>
+                <ListItemIcon className={classes.icon}>
+                  <AccountIcon />
+                </ListItemIcon>
+                <ListItemText
+                  classes={{ primary: classes.primary }}
+                  inset
+                  primary="Account"
+                />
+              </MenuItem>
+              <MenuItem className={classes.menuItem}>
+                <ListItemIcon className={classes.icon}>
+                  <AccountIcon />
+                </ListItemIcon>
+                <ListItemText
+                  classes={{ primary: classes.primary }}
+                  inset
+                  primary="History"
+                />
+              </MenuItem>
+            </MenuList>
+          </Paper>
+          <History netid={this.state.netid} />
+        </Grid>
       </Grid>
     );
   }
 }
 
-export default Account;
+export default withStyles(styles)(Account);
